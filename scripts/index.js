44dbb94a85058031fe2c3812a2e40223de49a43a
stevethats <stevethats@yahoo.com>
2024-07-09 06:24:00 -0500

    edit modal to recieve and save input

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,29 @@ let initialCards = [
 const editProfileEditButton = document.querySelector(".profile__edit-button");
 const editProfileBox = document.querySelector(".modal");
 const editProfileCloseButton = document.querySelector(".modal__close");
+const editProfileForm = document.querySelector(".modal__form");
+const profileTitle = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const editProfileTitle = editProfileBox.querySelector(".modal__form-title");
+const editProfileDescription = editProfileBox.querySelector(
+  ".modal__form-description"
+);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = editProfileTitle.value;
+  profileDescription.textContent = editProfileDescription.value;
+  editProfileBox.classList.remove("modal_opened");
+}
 
 editProfileEditButton.addEventListener("click", function () {
   editProfileBox.classList.add("modal_opened");
+  editProfileTitle.value = profileTitle.textContent;
+  editProfileDescription.value = profileDescription.textContent;
 });
 
 editProfileCloseButton.addEventListener("click", function () {
   editProfileBox.classList.remove("modal_opened");
 });
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
